de2a6e56afff0fa324dda74dbe2a82f804c89457
OMAR-GODDI <73104649+OMAR-GODDI@users.noreply.github.com>
2021-01-25 23:26:19 +0100

    update project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -122,9 +122,7 @@ function App() {
                   Admin <i className="fa fa-caret-down"></i>
                 </Link>
                 <ul className="dropdown-content">
-                  <li>
-                    <Link to="/dashboard">Dashboard</Link>
-                  </li>
+                  
                   <li>
                     <Link to="/productlist">Products</Link>
                   </li>
